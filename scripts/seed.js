23a75d8a418f2f6c181eb1a5cdd14dd0803e6a12
Matias Agelvis <Magelvisdoz@gmail.com>
2022-08-08 16:28:46 -0400

    Update DB Schema

--- scripts/seed.js
+++ scripts/seed.js
@@ -19,19 +19,19 @@ export default async () => {
     ]
     console.log(
       "\nUsing the default './scripts/seed.{js,ts}' template\nEdit the file to add seed data\n"
-    )
+    )()
 
     // Note: if using PostgreSQL, using `createMany` to insert multiple records is much faster
     // @see: https://www.prisma.io/docs/reference/api-reference/prisma-client-reference#createmany
-    Promise.all(
-      //
-      // Change to match your data model and seeding needs
-      //
-      data.map(async (data) => {
-        const record = await db.userExample.create({ data })
-        console.log(record)
-      })
-    )
+    // Promise
+    //   .all
+    //
+    // Change to match your data model and seeding needs
+    //
+    // data.map(async (data) => {
+    //   const record = await db.userExample.create({ data })
+    //   console.log(record)
+    // })
   } catch (error) {
     console.warn('Please define your seed data.')
     console.error(error)
